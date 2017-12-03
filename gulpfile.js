63d0f04b1ae9d6e936be9c24cb45dfc0d054168d
Vedegis Evgeny <tichoo@yandex.ru>
2017-12-03 14:26:50 +0300

    Add folder structure

--- gulpfile.js
+++ gulpfile.js
@@ -1,25 +1,43 @@
-var 	gulp     =      require('gulp'),
-        pug      = 		require('gulp-pug');
-        stylus   =		require('gulp-stylus');
+var 	gulp         =       require('gulp'),
+        pug          =       require('gulp-pug'),
+        stylus       =       require('gulp-stylus'),
+        browserSync  =       require('browser-sync');
 
-gulp.task('style', function(){
+//Stylus files
+gulp.task('styles', function(){
 	return gulp
-	.src('./dev/stylus/**/*.styl')    
-	.pipe (stylus()) 
-	.pipe (gulp.dest('./static/css/'))
+	.src('./dev/stylus/**/*.styl')
+	.pipe(stylus())
+	.pipe(gulp.dest('./build/static/css/'))
+    .pipe(browserSync.stream())
 });
 
+//Pug files
 gulp.task('pug', function () {
-	return gulp.src('./dev/*.pug')
- 	.pipe(pug())
- 	.pipe(gulp.dest('./static/'))
+	return gulp
+	.src('./dev/pug/*.pug')
+ 	.pipe(pug(
+        {
+            pretty: true
+        }
+    ))
+ 	.pipe(gulp.dest('./build/'))
 });
 
 
+//Server
+gulp.task('serve', function() {
+    browserSync.init({
+        server: "./build",
+        notify: false
+    })
+});
+
+//Watch
 gulp.task('watch', function(){
-	gulp.watch('./dev/stylus/**/*.styl', ['style']) 
-	gulp.watch('./dev/*.pug', ['pug']) 
+	gulp.watch('./dev/stylus/**/*.styl', ['styles']).on('change', browserSync.reload);
+	gulp.watch('./dev/pug/**/*.pug', ['pug']).on('change', browserSync.reload);
 });
 
 
-gulp.task('default', ['style','pug','watch']);
+gulp.task('default', ['serve','styles','pug','watch']);
