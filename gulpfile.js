3a6c804950691a00dfe9bb1cd7c85b9b0f10b4d9
Vedegis Evgeny <tichoo@yandex.ru>
2017-12-04 17:38:44 +0300

    Add DEL package

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,12 @@
 var     gulp         =       require('gulp'),
         pug          =       require('gulp-pug'),
         stylus       =       require('gulp-stylus'),
-        browserSync  =       require('browser-sync'),
         concat       =       require('gulp-concat'),
         uglify       =       require('gulp-uglifyjs'),
         cssnano      =       require('gulp-cssnano'),
-        rename       =       require('gulp-rename');
+        rename       =       require('gulp-rename'),
+        del          =       require('del'),
+        browserSync  =       require('browser-sync');
 
 //Pug files
 gulp.task('pug', function () {
@@ -20,8 +21,8 @@ gulp.task('pug', function () {
 
 });
 
-//Stylus files
-gulp.task('styles', function(){
+//Main stylus 
+gulp.task('style:main', function(){
     return gulp
     .src('./dev/stylus/main.styl')
     .pipe(browserSync.reload({stream: true}))
@@ -33,7 +34,7 @@ gulp.task('styles', function(){
 });
 
 //JS files
-gulp.task('scripts', function() {
+gulp.task('script:main', function() {
     return gulp
     .src('./dev/js/main.js')
     .pipe(browserSync.reload({stream: true}))
@@ -43,6 +44,10 @@ gulp.task('scripts', function() {
     .pipe(gulp.dest('./build/static/js'))
 });
 
+//Clean /build
+gulp.task('clean', function() {
+    return del.sync('build')
+});
 
 //Server
 gulp.task('serve', function() {
@@ -56,10 +61,11 @@ gulp.task('serve', function() {
 
 //Watch
 gulp.task('watch', function(){
-    gulp.watch('./dev/stylus/**/*.styl', ['styles']);
+    gulp.watch('./dev/stylus/**/*.styl', ['style:main']);
     gulp.watch('./dev/pug/**/*.pug', ['pug']);
-    gulp.watch('./dev/js/**/*.js', ['scripts']);
+    gulp.watch('./dev/js/**/*.js', ['script:main']);
 });
 
 
-gulp.task('default', ['styles','pug','scripts','serve','watch']);
+
+gulp.task('default', ['clean','style:main','pug','script:main','serve','watch']);
