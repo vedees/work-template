7991c0c500d49ef7e5010124cfe911dd8a79b5c8
Vedegis Evgeny <tichoo@yandex.ru>
2017-12-04 16:47:25 +0300

    Add uglifyjs, cssnano package

--- gulpfile.js
+++ gulpfile.js
@@ -1,28 +1,46 @@
-var 	gulp         =       require('gulp'),
+var     gulp         =       require('gulp'),
         pug          =       require('gulp-pug'),
         stylus       =       require('gulp-stylus'),
-        browserSync  =       require('browser-sync');
-
-//Stylus files
-gulp.task('styles', function(){
-	return gulp
-	.src('./dev/stylus/main.styl')
-	.pipe(stylus())
-	.pipe(gulp.dest('./build/static/css/'))
-    .pipe(browserSync.reload({stream: true}))
-});
+        browserSync  =       require('browser-sync'),
+        concat       =       require('gulp-concat'),
+        uglify       =       require('gulp-uglifyjs'),
+        cssnano      =       require('gulp-cssnano'),
+        rename       =       require('gulp-rename');
 
 //Pug files
 gulp.task('pug', function () {
     return gulp
-    .src('./dev/pug/pages/*.pug')
-    .pipe(pug({
-            pretty: true
-        }
-    ))
-    .pipe(gulp.dest('./build/'))
-    .pipe(browserSync.reload({stream: true}))
+        .src('./dev/pug/pages/*.pug')
+        .pipe(pug({
+                pretty: true
+            }
+        ))
+        .pipe(gulp.dest('./build/'))
+        .pipe(browserSync.reload({stream: true}))
 
+});
+
+//Stylus files
+gulp.task('styles', function(){
+    return gulp
+    .src('./dev/stylus/main.styl')
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(stylus())
+    .pipe(gulp.dest('./build/static/css/'))
+    .pipe(cssnano())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./build/static/css/'))
+});
+
+//JS files
+gulp.task('scripts', function() {
+    return gulp
+    .src('./dev/js/main.js')
+    .pipe(browserSync.reload({stream: true}))
+    .pipe(gulp.dest('./build/static/js'))
+    .pipe(uglify())
+    .pipe(rename({suffix: '.min'}))
+    .pipe(gulp.dest('./build/static/js'))
 });
 
 
@@ -32,15 +50,16 @@ gulp.task('serve', function() {
         server:{
             baseDir: 'build'
         },
-        notify: false
+        notify: false      //Disable notifications
     })
 });
 
 //Watch
 gulp.task('watch', function(){
-	gulp.watch('./dev/stylus/**/*.styl', ['styles']);
-	gulp.watch('./dev/pug/**/*.pug', ['pug']);
+    gulp.watch('./dev/stylus/**/*.styl', ['styles']);
+    gulp.watch('./dev/pug/**/*.pug', ['pug']);
+    gulp.watch('./dev/js/**/*.js', ['scripts']);
 });
 
 
-gulp.task('default', ['serve','styles','pug','watch']);
+gulp.task('default', ['styles','pug','scripts','serve','watch']);
