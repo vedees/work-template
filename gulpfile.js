79d4c17d304040d4f4e6417075b63fc3150993e8
Vedegis Evgeny <tichoo@yandex.ru>
2017-12-05 07:29:38 +0300

    Add css, js libs

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,10 @@ var     gulp         =       require('gulp'),
         uglify       =       require('gulp-uglifyjs'),
         cssnano      =       require('gulp-cssnano'),
         rename       =       require('gulp-rename'),
-        del          =       require('del'),
-        browserSync  =       require('browser-sync');
+        browserSync  =       require('browser-sync'),
+        del          =       require('del');
+
+
 
 //Pug files
 gulp.task('pug', function () {
@@ -21,33 +23,45 @@ gulp.task('pug', function () {
 
 });
 
+
 //Main stylus 
 gulp.task('style:main', function(){
     return gulp
     .src('./dev/stylus/main.styl')
     .pipe(browserSync.reload({stream: true}))
-    .pipe(stylus())
+    .pipe(stylus({
+            'include css': true
+        }
+    ))
     .pipe(gulp.dest('./build/static/css/'))
     .pipe(cssnano())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./build/static/css/'))
 });
 
+
 //JS files
 gulp.task('script:main', function() {
     return gulp
-    .src('./dev/js/main.js')
+    .src([
+        './node_modules/jquery/dist/jquery.js',
+     // './YOU_PATH/example.js',
+        './dev/js/common.js'
+    ])
     .pipe(browserSync.reload({stream: true}))
-    .pipe(gulp.dest('./build/static/js'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./build/static/js'))     //create main.js
     .pipe(uglify())
     .pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('./build/static/js'))
 });
 
+
 //Clean /build
 gulp.task('clean', function() {
     return del.sync('build')
 });
+
 
 //Server
 gulp.task('serve', function() {
@@ -58,6 +72,7 @@ gulp.task('serve', function() {
         notify: false      //Disable notifications
     })
 });
+
 
 //Watch
 gulp.task('watch', function(){
